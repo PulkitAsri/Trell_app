be62f47043dc1414d21549dbfa5cefe751e7baf1
PulkitAsri <pulkit.asri@gmail.com>
2021-04-22 16:37:31 +0530

    progress

--- app/src/main/java/com/example/trell_app/MainActivity.java
+++ app/src/main/java/com/example/trell_app/MainActivity.java
@@ -7,7 +7,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static Boolean started=false;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,18 +21,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mAuth=FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser()!=null){
+            Intent intent= new Intent(this , HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+
+        }else{
+            Intent intent= new Intent(this ,LogInActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+
+        }
 
 
         //delay of 2 sec and automatically to home ==> signup
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent=new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },2000);
         //to be replaced by automatic login
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
+
+
+
     }
 }
