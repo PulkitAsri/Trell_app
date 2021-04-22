b6677b83f56616f69382cb258aa3b998d95cea28
PulkitAsri <pulkit.asri@gmail.com>
2021-04-22 20:42:50 +0530

    added SignUp and Login functionality through Google Firebase.

--- app/src/main/java/com/example/trell_app/HomeActivity.java
+++ app/src/main/java/com/example/trell_app/HomeActivity.java
@@ -1,14 +1,20 @@
 package com.example.trell_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    RecyclerView mExploreTiles;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mExploreTiles=findViewById(R.id.exploreRecyclerView);
+
     }
 }
