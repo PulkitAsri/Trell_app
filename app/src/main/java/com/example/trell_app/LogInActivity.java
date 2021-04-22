f9d4764d355450c1607a61096f787785e9d8eadc
PulkitAsri <pulkit.asri@gmail.com>
2021-04-22 15:59:29 +0530

    added basic layouts to test login functionality

--- app/src/main/java/com/example/trell_app/LogInActivity.java
+++ app/src/main/java/com/example/trell_app/LogInActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class LogInActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
